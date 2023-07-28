09e096dc6a8d63fa515483b87fb7de0663c0d1be
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-07-27 23:07:17 -0500

    ver1

---
+++ Assets/Codigo/FollowPlayer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+public Transform PocisionCamara;
+public float plerp = 0.9f;
+void Update(){
+	transform.position = Vector3.Lerp(transform.position, PocisionCamara.position, plerp);
+}
+}
